33b22937f7c55986709819372744e753f7f2bce5
noreenakhan <noreenakhan@codeforpakistan.org>
2022-01-04 15:26:38 +0500

    Update Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -17,6 +17,7 @@ class Api extends CI_Controller {
     
     public function __construct()
 	{
+        $var_contructor_noreena = 0;
 		parent::__construct();
 		$this->load->model('AuthModel');
 		$this->load->model('ComplainantModel');
